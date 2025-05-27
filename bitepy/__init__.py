69f61302380d382cd37022425d4b38c32fb2f1d1
simon-hirsch <simon.hirsch@stud.uni-due.de>
2025-05-27 16:18:25 +0200

    Add version to init

--- bitepy/__init__.py
+++ bitepy/__init__.py
@@ -8,11 +8,16 @@
 # Licensed under MIT License, see https://opensource.org/license/mit
 ######################################################################
 
+from importlib.metadata import version
+
 from .simulation import Simulation
 from .data import Data
 from .results import Results
 
+
 __all__ = ["Simulation", "Data", "Results"]
+
+__version__ = version("bitepy")
 
 __doc__ = """
 bitepy: A Python Battery Intraday Trading Engine
